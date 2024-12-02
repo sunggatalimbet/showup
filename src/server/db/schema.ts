dcd70aba31088c2ae7a2b9413990c7c5ce7c5bea
Sunggat Alimbetov <sunggatalimbetov@gmail.com>
2024-12-03 00:11:41 +0500

    prettified

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,11 +3,11 @@
 
 import { sql } from "drizzle-orm";
 import {
-  index,
-  integer,
-  pgTableCreator,
-  timestamp,
-  varchar,
+    index,
+    integer,
+    pgTableCreator,
+    timestamp,
+    varchar,
 } from "drizzle-orm/pg-core";
 
 /**
@@ -19,18 +19,18 @@ import {
 export const createTable = pgTableCreator((name) => `showup_${name}`);
 
 export const posts = createTable(
-  "post",
-  {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+    "post",
+    {
+        id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+        name: varchar("name", { length: 256 }),
+        createdAt: timestamp("created_at", { withTimezone: true })
+            .default(sql`CURRENT_TIMESTAMP`)
+            .notNull(),
+        updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+            () => new Date()
+        ),
+    },
+    (example) => ({
+        nameIndex: index("name_idx").on(example.name),
+    })
 );
